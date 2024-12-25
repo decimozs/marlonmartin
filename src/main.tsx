39b4adbe9ac091d79c1a7d49b3167b6620c3012b
decimozs <marlonadiguemartin548@gmail.com>
2024-12-25 23:01:23 +0800

    chore: fix cursor

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import FollowCursor from "./components/core/cursor.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <FollowCursor />
     <App />
   </StrictMode>,
 );
